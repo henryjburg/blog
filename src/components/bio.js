3cc9c7acb0d0a1bc10edf1c0c658b04e80edf012
Henry Burgess <henryjburg@gmail.com>
2025-02-18 23:29:31 -0600

    Update profile picture

--- src/components/bio.js
+++ src/components/bio.js
@@ -31,7 +31,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile.jpeg"
+        src="../images/profile.jpg"
         width={50}
         height={50}
         quality={95}
